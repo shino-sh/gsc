555304f45a84e0d559a8de71c8bd1e8fc6af49eb
shino-sh <geassr2.ss@gmail.com>
2018-07-24 23:42:07 +0900

    add drag and drop polyfill

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
 import 'element-ui/lib/theme-chalk/index.css'
-import VueDragDrop from 'vue-drag-drop';
+import VueDragDrop from 'vue-drag-drop'
 import App from './App'
+import './assets/js/DragDropTouch'
 
 Vue.use(ElementUI, {locale})
 Vue.use(VueDragDrop)
