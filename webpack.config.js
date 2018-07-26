d3796501bcd567a8a2d570bed334c2c816ee3355
shino-sh <geassr2.ss@gmail.com>
2018-07-26 23:58:17 +0900

    make publicPath to relative

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: 'dist/',
     filename: 'build.js'
   },
   module: {
@@ -39,7 +39,10 @@ module.exports = {
       },
       { 
         test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/, 
-        loader: 'file-loader' 
+        loader: 'file-loader',
+        options: {
+          name: "fonts/[name].[ext]"
+        }
       }
     ]
   },
